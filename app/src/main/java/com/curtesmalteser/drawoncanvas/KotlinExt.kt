093c17c38a5b08c5f3c8dd0d2aa5960c6350510b
curtes_malteser <antonio_bastiao@hotmail.com>
2019-04-11 23:18:11 +0200

    missing changes

--- app/src/main/java/com/curtesmalteser/drawoncanvas/KotlinExt.kt
+++ app/src/main/java/com/curtesmalteser/drawoncanvas/KotlinExt.kt
@@ -1,6 +1,8 @@
 package com.curtesmalteser.drawoncanvas
 
 import android.content.res.Resources
+import android.graphics.Paint
+import android.graphics.SweepGradient
 import android.util.TypedValue
 import android.view.View
 
@@ -13,6 +15,14 @@ fun Resources.fontSPtoPixels(dimen: Int): Int {
             displayMetrics).toInt()
 }
 
+fun Resources.convertDpIntoPixel(dp: Float): Float {
+    return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            displayMetrics
+    )
+}
+
 ///////////////////////
 /////// ViewExt ///////
 ///////////////////////
@@ -21,3 +31,24 @@ fun View.findCenter(): Center {
 }
 
 data class Center(val x: Float, val y: Float)
+
+////////////////////////
+/////// PaintExt ///////
+////////////////////////
+
+fun Paint.myPaint(color: Int, style: Paint.Style, strokeWidth: Float) {
+    this.isAntiAlias = true
+    this.color = color
+    this.style = style
+    this.strokeWidth = strokeWidth
+}
+
+fun Paint.makeRadGrad(strokeWidth: Float, cx: Float, cy: Float, listColors: List<Int>, positionsList: List<Float>?) {
+    val gradient = SweepGradient(cx, cy, listColors.toIntArray(), positionsList?.toFloatArray())
+
+    this.strokeWidth = strokeWidth
+    this.style = Paint.Style.STROKE
+    this.isDither = true
+    this.shader = gradient
+}
+
